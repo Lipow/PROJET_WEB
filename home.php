f6e3f75fdcc833a7c8e429b77dbaba936cb8d13d
skiou <jeremy.mortelette@gmail.com>
2017-01-26 10:16:34 +0100

    first maj

--- home.php
+++ home.php
@@ -1,3 +1,18 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ConnectLife-Home</title>
+  </head>
+  <body>
+    <div class="Message">
+      <h1>Bienvenu,</h1>
+        <p>Dans un soucis de qualité, notre société souhaite procéder à une mise à jour de son fichier client.<br>
+           Nous apprécions le temps que vous nous accordez pour répondre à notre formulaire et pour vous<br>
+           remercier, nous vous offrons on <strong>bon d'achat!!</strong>
+        </p>
+    </div>
+        <button id="Form" type="button" name="Formulaire">Formulaire de mise à jour</button>
+        <button id="Form" onclick="lien formulaire">Formulaire de mise à jour</button>
+  </body>
+</html>
